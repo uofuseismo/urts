bf30263a37a6ea34330b237a4c657e3a8a35a28b
Ben Baker <bakerb845@gmail.com>
2023-01-13 11:44:51 -0700

    adding libslink cmake

---
+++ cmake/FindSEEDLinkConfig.cmake
@@ -0,0 +1,26 @@
+# Already in cache, be silent
+if (SEEDLINK_INCLUDE_DIR AND SEEDLINK_LIBRARY)
+    set(SEEDLINK_FIND_QUIETLY TRUE)
+endif()
+
+# Find the include directory
+find_path(SEEDLINK_INCLUDE_DIR
+          NAMES libslink.h
+          HINTS $ENV{SEEDLINK_ROOT}/include
+                $ENV{SEEDLINK_ROOT}/
+                /usr/local/include
+          )
+# Find the library components
+find_library(SEEDLINK_LIBRARY
+             NAMES slink
+             PATHS $ENV{SEEDLINK_ROOT}/lib/
+                   $ENV{SEEDLINK_ROOT}/
+                   /usr/local/lib64
+                   /usr/local/lib
+            )
+# Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if
+# all listed variables are TRUE.
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(FindSEEDLink
+                                  DEFAULT_MSG SEEDLINK_INCLUDE_DIR SEEDLINK_LIBRARY)
+mark_as_advanced(SEEDLINK_INCLUDE_DIR SEEDLINK_LIBRARY)
