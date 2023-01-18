4d1c82e061214edc581c9802bb69bee2407da5e9
Ben Baker <u6024448@utah.edu>
2023-01-18 09:42:09 -0700

    was including wrong version information

--- src/version.cpp
+++ src/version.cpp
@@ -1,36 +1,36 @@
 #include <string>
-#include "umps/version.hpp"
+#include "urts/version.hpp"
 
-using namespace UMPS;
+using namespace URTS;
 
 int Version::getMajor() noexcept
 {
-    return UMPS_MAJOR;
+    return URTS_MAJOR;
 }
 
 int Version::getMinor() noexcept
 {
-    return UMPS_MINOR;
+    return URTS_MINOR;
 }
 
 int Version::getPatch() noexcept
 {
-    return UMPS_PATCH;
+    return URTS_PATCH;
 }
 
 bool Version::isAtLeast(const int major, const int minor,
                         const int patch) noexcept
 {
-    if (UMPS_MAJOR < major){return false;}
-    if (UMPS_MAJOR > major){return true;}
-    if (UMPS_MINOR < minor){return false;}
-    if (UMPS_MINOR > minor){return true;}
-    if (UMPS_PATCH < patch){return false;}
+    if (URTS_MAJOR < major){return false;}
+    if (URTS_MAJOR > major){return true;}
+    if (URTS_MINOR < minor){return false;}
+    if (URTS_MINOR > minor){return true;}
+    if (URTS_PATCH < patch){return false;}
     return true;
 }
 
 std::string Version::getVersion() noexcept
 {
-    std::string version(UMPS_VERSION);
+    std::string version(URTS_VERSION);
     return version;
 }
