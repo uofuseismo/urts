d1c6ff0b177e05e4454b94fa688d80887375b536
Ben Baker <bakerb845@gmail.com>
2023-02-13 16:01:59 -0700

    adding a client

---
+++ include/urts/services/scalable/detectors/uNetThreeComponentP/requestor.hpp
@@ -0,0 +1,129 @@
+#ifndef URTS_SERVICES_SCALABLE_DETECTORS_UNET_THREE_COMPONENT_P_REQUESTOR_HPP
+#define URTS_SERVICES_SCALABLE_DETECTORS_UNET_THREE_COMPONENT_P_REQUESTOR_HPP
+#include <memory>
+// Forward declarations
+namespace UMPS
+{
+ namespace Logging
+ {
+  class ILog;
+ }
+ namespace Messaging
+ {
+  class Context; 
+ }
+}
+namespace URTS::Services::Scalable::Detectors::UNetThreeComponentP
+{
+ class RequestorOptions;
+ class InferenceRequest;
+ class InferenceResponse;
+ class PreprocessingRequest;
+ class PreprocessingResponse;
+ class ProcessingRequest;
+ class ProcessingResponse;
+}
+namespace URTS::Services::Scalable::Detectors::UNetThreeComponentP
+{
+/// @class Requestor "requestor.hpp" "urts/services/scalable/detectors/uNetThreeComponentP/requestor.hpp"
+/// @brief A requestor that will interact with the inference service.
+/// @copyright Ben Baker (University of Utah) distributed under the MIT license.
+class Requestor
+{
+public:
+    /// @name Constructors
+    /// @{
+
+    /// @brief Constructor.
+    Requestor();
+    /// @brief Constructor with a given logger.
+    /// @param[in] logger  The logger.
+    explicit Requestor(std::shared_ptr<UMPS::Logging::ILog> &logger);
+    /// @brief Constructor with a given context.
+    /// @param[in] context  The ZeroMQ context to use.
+    explicit Requestor(std::shared_ptr<UMPS::Messaging::Context> &context);
+    /// @brief Constructor with a given context and logger.
+    Requestor(std::shared_ptr<UMPS::Messaging::Context> &context,
+              std::shared_ptr<UMPS::Logging::ILog> &logger);
+    /// @brief Move constructor.
+    /// @param[in,out] requestor  The request class from which to initialize
+    ///                           this class.  On exit, request's behavior is
+    ///                           undefined.
+    Requestor(Requestor &&requestor) noexcept;
+    /// @}
+
+    /// @name Operators
+    /// @{
+
+    /// @brief Move assignment operator.
+    /// @param[in,out] requestor  The request class whose memory will be moved
+    ///                           to this.  On exit, request's behavior is
+    ///                           undefined.
+    Requestor& operator=(Requestor &&requestor) noexcept;
+    /// @result The memory from request moved to this.
+    /// @} 
+
+    /// @name Step 1: Initialization
+    /// @{
+
+    /// @brief Initializes the request.
+    /// @param[in] options   The request options.
+    /// @throws std::invalid_argument if the endpoint.
+    void initialize(const RequestorOptions &options);
+    /// @result True indicates the class is initialized.
+    [[nodiscard]] bool isInitialized() const noexcept;
+    /// @}
+
+    /// @name Step 2: Request 
+    /// @{
+
+    /// @brief Performs a blocking processing (preprocessing followed by 
+    ///        inference) request.
+    /// @param[in] request  The processing request to make to the
+    ///                     the server via the router.
+    /// @result The response to the processing request.
+    /// @throws std::invalid_argument if there are no data requests in the
+    ///         bulk request.
+    /// @throws std::runtime_error if \c isInitialized() is false.
+    /// @note This is faster than performing a preprocessing then inference
+    ///       request.
+    [[nodiscard]] std::unique_ptr<ProcessingResponse> request(const ProcessingRequest &request);
+    /// @brief Performs a blocking inference request.
+    /// @param[in] request  The inference request to make to the server via
+    ///                     the router. 
+    /// @result The response to the inference request from the server.
+    /// @throws std::runtime_error if \c isInitialized() is false.
+    [[nodiscard]] std::unique_ptr<InferenceResponse> request(const InferenceRequest &request);
+    /// @brief Performs a blocking data preprocessing request.
+    /// @param[in] request  The preprocessing request to make to the server via
+    ///                     the router.
+    /// @result The response to the preprocessing request from the server.
+    ///         This should contain data that can be used to form an
+    ///         inference request.
+    /// @throws std::runtime_error if \c isInitialized() is false.
+    [[nodiscard]] std::unique_ptr<PreprocessingResponse> request(const PreprocessingRequest &request);
+    /// @}
+
+    /// @name Step 3: Disconnecting
+    /// @{
+
+    /// @brief Disconnects the requestor from the router-dealer.
+    /// @note This step is optional as it will be done by the destructor.
+    void disconnect();
+    /// @}
+ 
+    /// @name Destructors
+    /// @{
+
+    /// @brief Destructor.
+    ~Requestor();
+    /// @}
+
+    Requestor(const Requestor &request) = delete;
+    Requestor& operator=(const Requestor &request) = delete;
+private:
+    class RequestorImpl;
+    std::unique_ptr<RequestorImpl> pImpl;
+};
+}
+#endif
--- testing/services/scalable/detectors/uNetThreeComponentP.cpp
+++ testing/services/scalable/detectors/uNetThreeComponentP.cpp
@@ -5,12 +5,16 @@
 #include <umps/authentication/zapOptions.hpp>
 #include <umps/messaging/routerDealer/proxy.hpp>
 #include <umps/messaging/routerDealer/proxyOptions.hpp>
+#include <uussmlmodels/detectors/uNetThreeComponentP/inference.hpp>
+#include <uussmlmodels/detectors/uNetThreeComponentP/preprocessing.hpp>
 #include "urts/services/scalable/detectors/uNetThreeComponentP/inferenceRequest.hpp"
 #include "urts/services/scalable/detectors/uNetThreeComponentP/inferenceResponse.hpp"
 #include "urts/services/scalable/detectors/uNetThreeComponentP/preprocessingRequest.hpp"
 #include "urts/services/scalable/detectors/uNetThreeComponentP/preprocessingResponse.hpp"
 #include "urts/services/scalable/detectors/uNetThreeComponentP/processingRequest.hpp"
 #include "urts/services/scalable/detectors/uNetThreeComponentP/processingResponse.hpp"
+#include "urts/services/scalable/detectors/uNetThreeComponentP/requestorOptions.hpp"
+#include "urts/services/scalable/detectors/uNetThreeComponentP/requestor.hpp"
 #include "urts/services/scalable/detectors/uNetThreeComponentP/serviceOptions.hpp"
 #include "urts/services/scalable/detectors/uNetThreeComponentP/service.hpp"
 #include <gtest/gtest.h>
@@ -363,11 +367,56 @@ TEST(ServicesScalableDetectorsUNetThreeComponentP, ServiceOptions)
               UMPS::Authentication::SecurityLevel::Grasslands);
 }
 
+TEST(ServicesScalableDetectorsUNetThreeComponentP, RequestorOptions)
+{
+    const std::string address{"tcp://127.0.0.1:5550"};
+    const int sendHWM{105};
+    const int recvHWM{106};
+    const std::chrono::milliseconds sendTimeOut{120};
+    const std::chrono::milliseconds recvTimeOut{145};
+    UMPS::Authentication::ZAPOptions zapOptions;
+    zapOptions.setStrawhouseClient();
+
+    RequestorOptions options;
+    EXPECT_NO_THROW(options.setAddress(address));
+    EXPECT_NO_THROW(options.setSendHighWaterMark(sendHWM));
+    EXPECT_NO_THROW(options.setReceiveHighWaterMark(recvHWM));
+    options.setSendTimeOut(sendTimeOut);
+    options.setReceiveTimeOut(recvTimeOut);
+    options.setZAPOptions(zapOptions);
+
+    RequestorOptions copy(options);
+    EXPECT_EQ(copy.getAddress(), address);
+    EXPECT_EQ(copy.getSendHighWaterMark(), sendHWM);
+    EXPECT_EQ(copy.getReceiveHighWaterMark(), recvHWM);
+    EXPECT_EQ(copy.getSendTimeOut(), sendTimeOut); 
+    EXPECT_EQ(copy.getReceiveTimeOut(), recvTimeOut);
+    EXPECT_EQ(copy.getZAPOptions().getSecurityLevel(),
+              zapOptions.getSecurityLevel());
+
+    options.clear();
+    EXPECT_EQ(options.getSendHighWaterMark(), 4096);
+    EXPECT_EQ(options.getReceiveHighWaterMark(), 8192);
+    EXPECT_EQ(options.getSendTimeOut(), std::chrono::milliseconds {0});
+    EXPECT_EQ(options.getReceiveTimeOut(), std::chrono::milliseconds {3000});
+    zapOptions.setGrasslandsClient();
+    EXPECT_EQ(options.getZAPOptions().getSecurityLevel(),
+              zapOptions.getSecurityLevel());
+}
+
 ///--------------------------------------------------------------------------///
 ///                               Communication Test                         ///
 ///--------------------------------------------------------------------------///
 const std::string frontendAddress{"tcp://127.0.0.1:5555"};
 const std::string backendAddress{"tcp://127.0.0.1:5556"};
+const std::string weightsFile{
+    "/usr/local/share/UUSSMLModels/detectorsUNetThreeComponentP.onnx"
+}; 
+std::vector<double> zProcRef, nProcRef, eProcRef, pRef, pRefWindow;
+std::vector<double> vertical(1008, 0); 
+std::vector<double> north(1008, -1);
+std::vector<double> east(1008, 1); 
+
 
 void proxy()
 {
@@ -380,16 +429,13 @@ void proxy()
                             &proxy);
     std::this_thread::sleep_for(std::chrono::milliseconds{10});
     EXPECT_TRUE(proxy.isRunning());
-    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
     EXPECT_NO_THROW(proxy.stop());
     proxyThread.join();
 }
 
 void server()
 {
-    const std::string weightsFile{
-        "/usr/local/share/UUSSMLModels/detectorsUNetThreeComponentP.onnx"
-    };
     ServiceOptions options;
     options.setAddress(backendAddress);
     options.setModelWeightsFile(weightsFile);
@@ -398,40 +444,164 @@ void server()
     std::this_thread::sleep_for(std::chrono::milliseconds{10});
     EXPECT_TRUE(service.isInitialized());
     EXPECT_NO_THROW(service.start());
-    std::this_thread::sleep_for(std::chrono::milliseconds{1100});
+    std::this_thread::sleep_for(std::chrono::milliseconds{2000});
     service.stop();
 }
 
 void client()
 {
-/*
     RequestorOptions options;
     options.setAddress(frontendAddress);
     Requestor client;
     EXPECT_NO_THROW(client.initialize(options));
     EXPECT_TRUE(client.isInitialized());
-*/
-/*
-    ItemsRequest itemsRequest;
-    itemsRequest.setIdentifier(1);
-    // Make sure all the items are there
-    auto referenceItems = IncrementRequest::getItemSet(); 
-    auto itemsReply = client.request(itemsRequest);
-    if (itemsReply != nullptr)
-    {   
-        EXPECT_EQ(itemsReply->getReturnCode(),
-                  ItemsResponse::ReturnCode::Success);
-        auto names = itemsReply->getItems();
-        for (const auto &name : referenceItems)
+
+    const double samplingRate{100};
+
+    // Do a processing request
+    PreprocessingRequest preprocessingRequest;
+    preprocessingRequest.setIdentifier(2);
+    EXPECT_NO_THROW(preprocessingRequest.setSamplingRate(samplingRate));
+    EXPECT_NO_THROW(preprocessingRequest.setVerticalNorthEastSignal(vertical,
+                                                                    north,
+                                                                    east));
+    std::unique_ptr<PreprocessingResponse> preprocessingResponse{nullptr};
+    EXPECT_NO_THROW(preprocessingResponse
+        = client.request(preprocessingRequest));
+    std::vector<double> zProc, nProc, eProc;
+    if (preprocessingResponse != nullptr)
+    {
+        EXPECT_EQ(preprocessingResponse->getIdentifier(), 2);
+        zProc = preprocessingResponse->getVerticalSignal();
+        nProc = preprocessingResponse->getNorthSignal();
+        eProc = preprocessingResponse->getEastSignal();
+        EXPECT_EQ(zProc.size(), zProcRef.size());
+        double error = 0;
+        for (int i = 0; i < static_cast<int> (zProc.size()); ++i)
         {
-            EXPECT_TRUE(names.contains(IncrementRequest::itemToString(name)));
+            error = std::max(error, std::abs(zProc.at(i) - zProcRef.at(i)));
+            error = std::max(error, std::abs(nProc.at(i) - nProcRef.at(i)));
+            error = std::max(error, std::abs(eProc.at(i) - eProcRef.at(i)));
+        } 
+        EXPECT_NEAR(error, 0, 1.e-7);
+    }
+    // Follow on with an inference test
+    InferenceRequest inferenceRequest;
+    inferenceRequest.setIdentifier(3);
+    inferenceRequest.setVerticalNorthEastSignal(zProcRef, nProcRef, eProcRef,
+        InferenceRequest::InferenceStrategy::SlidingWindow);
+    std::unique_ptr<InferenceResponse> inferenceResponse{nullptr};
+    EXPECT_NO_THROW(
+         inferenceResponse = client.request(inferenceRequest)
+    );
+    if (inferenceResponse != nullptr)
+    {
+        EXPECT_EQ(inferenceResponse->getIdentifier(), 3);
+        auto probabilitySignal = inferenceResponse->getProbabilitySignal();
+        EXPECT_EQ(probabilitySignal.size(), pRefWindow.size());
+        double error = 0;
+        for (int i = 0; i < static_cast<int> (probabilitySignal.size()); ++i)
+        {
+            error = std::max(error, std::abs(probabilitySignal[i]
+                                           - pRefWindow[i]));
         }
-    }   
-    else
-    {   
-        EXPECT_TRUE(false);
-    }   
-*/
+        EXPECT_NEAR(error, 0, 1.e-7);
+    }
+    // Repeat with fixed window
+    inferenceRequest.setVerticalNorthEastSignal(zProcRef, nProcRef, eProcRef,
+        InferenceRequest::InferenceStrategy::FixedWindow);
+    EXPECT_NO_THROW(
+         inferenceResponse = client.request(inferenceRequest)
+    );
+    if (inferenceResponse != nullptr)
+    {
+        EXPECT_EQ(inferenceResponse->getIdentifier(), 3); 
+        auto probabilitySignal = inferenceResponse->getProbabilitySignal();
+        EXPECT_EQ(probabilitySignal.size(), pRef.size());
+        double error = 0;
+        for (int i = 0; i < static_cast<int> (probabilitySignal.size()); ++i)
+        {
+            error = std::max(error, std::abs(probabilitySignal[i]
+                                           - pRef[i]));
+        }
+        EXPECT_NEAR(error, 0, 1.e-7);
+    }
+
+
+    // Do a sliding window (usual) then fixed window test
+    ProcessingRequest processingRequest;
+    processingRequest.setIdentifier(1);
+    EXPECT_NO_THROW(processingRequest.setSamplingRate(samplingRate));
+    EXPECT_NO_THROW(
+        processingRequest.setVerticalNorthEastSignal(vertical, north, east,
+            ProcessingRequest::InferenceStrategy::SlidingWindow)
+    );
+    std::unique_ptr<ProcessingResponse> processingResponse{nullptr};
+    EXPECT_NO_THROW(processingResponse = client.request(processingRequest));
+    if (processingResponse != nullptr)
+    {
+        EXPECT_EQ(processingResponse->getIdentifier(), 1); 
+        auto probabilitySignal = processingResponse->getProbabilitySignal();
+        double error = 0;
+        for (int i = 0; i < static_cast<int> (probabilitySignal.size()); ++i)
+        {
+            error = std::max(error, std::abs(probabilitySignal[i]
+                                           - pRefWindow[i]));
+        }
+        EXPECT_NEAR(error, 0, 1.e-7);
+    }
+    EXPECT_NO_THROW(
+        processingRequest.setVerticalNorthEastSignal(vertical, north, east,
+            ProcessingRequest::InferenceStrategy::FixedWindow)
+    );
+    EXPECT_NO_THROW(processingResponse = client.request(processingRequest));
+    if (processingResponse != nullptr)
+    {
+        auto probabilitySignal = processingResponse->getProbabilitySignal();
+        EXPECT_EQ(processingResponse->getIdentifier(), 1);
+        EXPECT_EQ(pRef.size(), probabilitySignal.size());
+        double error = 0; 
+        for (int i = 0; i < static_cast<int> (probabilitySignal.size()); ++i)
+        {
+            error = std::max(error, std::abs(probabilitySignal[i]
+                                           - pRef[i]));
+        }
+        EXPECT_NEAR(error, 0, 1.e-7);
+    }
+}
+
+TEST(ServicesScalableDetectorsUNetThreeComponentP, Service)
+{
+    // Do this manually
+    {
+        UUSSMLModels::Detectors::UNetThreeComponentP::Preprocessing
+             preprocessing;
+        UUSSMLModels::Detectors::UNetThreeComponentP::Inference inference;
+        EXPECT_NO_THROW(inference.load(weightsFile));
+
+        auto result = preprocessing.process(vertical, north, east);
+        zProcRef = std::get<0> (result);
+        nProcRef = std::get<1> (result);
+        eProcRef = std::get<2> (result);
+        pRefWindow = inference.predictProbabilitySlidingWindow(zProcRef,
+                                                               nProcRef,
+                                                               eProcRef);
+        pRef = inference.predictProbability(zProcRef, nProcRef, eProcRef);
+    }
+    // Start the proxy
+    auto proxyThread = std::thread(proxy);
+    std::this_thread::sleep_for(std::chrono::milliseconds {50});
+    // Connect / initialize the server
+    auto serverThread = std::thread(server);
+    // Let the clients rip
+    std::this_thread::sleep_for(std::chrono::milliseconds {500});
+    auto clientThread1 = std::thread(client);
+    auto clientThread2 = std::thread(client);
+
+    clientThread1.join();
+    clientThread2.join();
+    serverThread.join();
+    proxyThread.join();
 }
 
 }
