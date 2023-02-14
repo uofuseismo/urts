df94896bc5865beefd55b310b2127609df89345d
Ben Baker <bakerb845@gmail.com>
2023-02-14 12:30:03 -0700

    adding postgres connection

---
+++ include/urts/database/connection/postgresql.hpp
@@ -0,0 +1,160 @@
+#ifndef URTS_DATABASE_CONNECTION_POSTGRESQL_HPP
+#define URTS_DATABASE_CONNECTION_POSTGRESQL_HPP
+#include <memory>
+namespace soci
+{
+ class session;
+}
+namespace URTS::Database::Connection
+{
+/// @name PostgreSQL "postgresql.hpp" "urts/database/connection/postgresql.hpp"
+/// @brief Defines a PostGresSQL connection.
+/// @copyright Ben Baker (University of Utah) distributed under the MIT license. 
+class PostgreSQL
+{
+public:
+    /// @name Constructors
+    /// @{
+
+    /// @brief Constructor.
+    PostgreSQL();
+    /// @brief Move constructor.
+    /// @param[in,out] connection  The connection from which to initialize this
+    ///                            class.
+    PostgreSQL(PostgreSQL &&connection) noexcept;
+    /// @}
+
+    /// @name Operators
+    /// @{
+
+    /// @brief Move assignment.
+    /// @param[in,out] connection  The connection whose memory will be moved to
+    ///                            this.  On exit, connection's behavior is
+    ///                            is undefined.
+    /// @result The memory from the connection moved to this.
+    PostgreSQL& operator=(PostgreSQL &&connection) noexcept;
+    /// @}
+    
+    /// @name User Name
+    /// @{
+    /// @brief Sets the user name.
+    /// @param[in] user  The user name.
+    /// @throws std::invalid_argument if user is empty.
+    void setUser(const std::string &user);
+    /// @result The user name.
+    /// @throws std::runtime_error if \c haveUser() is false.
+    [[nodiscard]] std::string getUser() const;
+    /// @result True indicates the user name was set.
+    [[nodiscard]] bool haveUser() const noexcept;
+    /// @}
+
+    /// @name Password
+    /// @{
+
+    /// @brief Sets the user's password.
+    /// @param[in] password  The user's password.
+    /// @throws std::invalid_argument if password is empty.
+    void setPassword(const std::string &password);
+    /// @result The user's password.
+    /// @throws std::runtime_error if \c havePassword() is false.
+    [[nodiscard]] std::string getPassword() const;
+    /// @result True indicates the user's password was set.
+    [[nodiscard]] bool havePassword() const noexcept;
+    /// @}
+
+    /// @name Database Host Address
+    /// @{
+
+    /// @brief Sets the host's address.
+    /// @param[in] address  The hosts's address e.g., machine.domain.com
+    /// @throws std::invalid_argument if the address is empty.
+    void setAddress(const std::string &address);
+    /// @result The host address.
+    /// @note By default this is 127.0.0.1
+    [[nodiscard]] std::string getAddress() const noexcept;
+    /// @}
+
+    /// @name Database Name
+    /// @{
+
+    /// @brief Sets the name of the database.
+    /// @throws std::invalid_argument if name is empty.
+    void setDatabaseName(const std::string &name);
+    /// @result The name of the database to which to connect.
+    /// @throw std::runtime_error if \c haveDatabasename() is false.
+    [[nodiscard]] std::string getDatabaseName() const;
+    /// @result True indicates the database name was set.
+    [[nodiscard]] bool haveDatabaseName() const noexcept;
+    /// @}
+
+    /// @name Port Number
+    /// @{
+
+    /// @brief Sets the port number.
+    void setPort(int port);
+    /// @result The port number.  By default this is 5432.
+    [[nodiscard]] int getPort() const noexcept;
+    /// @}
+
+    /// @name Application Name
+    /// @{
+
+    /// @brief Sets the name of the application.
+    /// @param[in] application  The name of the application.
+    /// @throws std::invalid_argument if the application is empty.
+    void setApplication(const std::string &application);
+    /// @result The application name.
+    /// @note By default this is qurts.
+    [[nodiscard]] std::string getApplication() const noexcept;
+    /// @}
+
+    /// @name Driver
+    /// @{
+
+    /// @result The driver (e.g., postgresql).
+    [[nodiscard]] static std::string getDriver() noexcept;
+    /// @}
+
+    /// @name Connect
+    /// @{
+
+    /// @result The connection string.
+    [[nodiscard]] std::string getConnectionString() const;
+    /// @brief Establishes a connection from the above resources.
+    void connect();
+    /// @result True indicates the connection was established.
+    [[nodiscard]] bool isConnected() const noexcept;
+    /// @}
+
+    /// @result A shared pointer to the session.
+    /// @throws std::runtime_error if \c isConnected() is false.
+    soci::session *getSession() const;
+
+    /// @brief Loads the database connection information from an
+    ///        initialization file.
+    /// @param[in] fileName  The name of the initialization file.
+    /// @param[in] section   The section of the initialization file
+    ///                      with the requisite connection information.
+    /// @throws std::invalid_argument if fileName cannot be found or
+    ///         information cannot be found.
+    /// @note The user and password, if not specified, will be extracted
+    ///       from the URTS_AQMS_RDONLY_USER and URTS_AQMS_RDONLY_PASSWORD
+    ///       environment variables, respectively.
+    void parseInitializationFile(const std::string &fileName,
+                                 const std::string &section = "AQMSPostgreSQL");
+
+    /// @name Destructors
+    /// @{
+
+    /// @brief Destructor.
+    ~PostgreSQL();
+    /// @}
+
+    PostgreSQL(const PostgreSQL &pg) = delete;
+    PostgreSQL& operator=(const PostgreSQL &pg) = delete;
+private:
+    class PostgreSQLImpl;
+    std::unique_ptr<PostgreSQLImpl> pImpl;
+};
+}
+#endif
---
+++ src/database/connection/postgresql.cpp
@@ -0,0 +1,247 @@
+#include <string>
+#include <soci/soci.h>
+#include <soci/postgresql/soci-postgresql.h>
+#include <boost/program_options.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <filesystem>
+#include "urts/database/connection/postgresql.hpp"
+
+using namespace URTS::Database::Connection;
+
+class PostgreSQL::PostgreSQLImpl
+{
+public:
+    soci::session mSession;
+    std::string mConnectionString;
+    std::string mUser{std::string(std::getenv("URTS_AQMS_RDONLY_USER"))};
+    std::string mPassword{
+        std::string(std::getenv("URTS_AQMS_RDONLY_PASSWORD"))};
+    std::string mDatabaseName;
+    std::string mAddress{"127.0.0.1"};
+    std::string mApplication{"urts"};
+    int mPort{5432};
+};
+
+/// C'tor
+PostgreSQL::PostgreSQL() :
+    pImpl(std::make_unique<PostgreSQLImpl> ())
+{
+}
+
+/// Move c'tor
+PostgreSQL::PostgreSQL(PostgreSQL &&pg) noexcept
+{
+    *this = std::move(pg);
+}
+
+/// Move assignment
+PostgreSQL& PostgreSQL::operator=(PostgreSQL &&pg) noexcept
+{
+    if (&pg == this){return *this;}
+    pImpl = std::move(pg.pImpl);
+    return *this;
+}
+
+/// Destructor
+PostgreSQL::~PostgreSQL() = default; 
+
+/// User
+void PostgreSQL::setUser(const std::string &user)
+{
+    if (user.empty())
+    {
+        throw std::invalid_argument("user is empty");
+    }
+    pImpl->mConnectionString.clear();
+    pImpl->mUser = user;
+}
+
+std::string PostgreSQL::getUser() const
+{
+    if (!haveUser()){throw std::runtime_error("User not set");}
+    return pImpl->mUser;
+}
+
+bool PostgreSQL::haveUser() const noexcept
+{
+    return !pImpl->mUser.empty();
+}
+
+/// Password
+void PostgreSQL::setPassword(const std::string &password)
+{
+    if (password.empty())
+    {
+        throw std::invalid_argument("Password is empty");
+    }
+    pImpl->mConnectionString.clear();
+    pImpl->mPassword = password;
+}
+
+std::string PostgreSQL::getPassword() const
+{
+    if (!havePassword()){throw std::runtime_error("Password not set");}
+    return pImpl->mPassword;
+}
+
+bool PostgreSQL::havePassword() const noexcept
+{
+    return !pImpl->mPassword.empty();
+}
+
+/// Address
+void PostgreSQL::setAddress(const std::string &address)
+{
+    if (address.empty())
+    {
+        throw std::invalid_argument("Address is empty");
+    }
+    pImpl->mConnectionString.clear();
+    pImpl->mAddress = address;
+}
+
+std::string PostgreSQL::getAddress() const noexcept
+{
+    return pImpl->mAddress;
+}
+
+/// DB name
+void PostgreSQL::setDatabaseName(const std::string &name)
+{
+    if (name.empty())
+    {
+        throw std::invalid_argument("Name is empty");
+    }
+    pImpl->mConnectionString.clear();
+    pImpl->mDatabaseName = name;
+}
+
+std::string PostgreSQL::getDatabaseName() const
+{
+    if (!haveDatabaseName()){throw std::runtime_error("Database name not set");}
+    return pImpl->mDatabaseName;
+}
+
+bool PostgreSQL::haveDatabaseName() const noexcept
+{
+    return !pImpl->mDatabaseName.empty();
+}
+
+/// Port
+void PostgreSQL::setPort(const int port)
+{
+    if (port < 0){throw std::invalid_argument("Port cannot be negative");}
+    pImpl->mConnectionString.clear();
+    pImpl->mPort = port;
+}
+
+int PostgreSQL::getPort() const noexcept
+{
+    return pImpl->mPort;
+}
+
+/// Application
+void PostgreSQL::setApplication(const std::string &application)
+{
+    if (application.empty())
+    {
+        throw std::invalid_argument("Application is empty");
+    }
+    pImpl->mConnectionString.clear();
+    pImpl->mApplication = application;
+}
+
+std::string PostgreSQL::getApplication() const noexcept
+{
+    return pImpl->mApplication;
+}
+
+/// Drivername
+std::string PostgreSQL::getDriver() noexcept
+{
+   return "postgresql";
+}
+
+/// Generate a connection string
+std::string PostgreSQL::getConnectionString() const
+{
+    if (!pImpl->mConnectionString.empty()){return pImpl->mConnectionString;}
+    if (!haveUser()){throw std::runtime_error("User not set");}
+    if (!havePassword()){throw std::runtime_error("Password not set");}
+    auto user = getUser();
+    auto password = getPassword();
+    auto address = getAddress();
+    auto cPort = std::to_string(getPort()); 
+    auto dbname = getDatabaseName();
+    auto appName = getApplication();
+    pImpl->mConnectionString = //getDriver()
+                             + "postgresql://" + user
+                             + ":" + password
+                             + "@" + address
+                             + ":" + cPort
+                             + "/" + dbname
+                             + "?connect_timeout=10"
+                             + "&application_name=" + appName;
+    return pImpl->mConnectionString;
+}
+
+/// Connect
+void PostgreSQL::connect()
+{
+    auto connectionString = getConnectionString(); // Throws
+    if (pImpl->mSession.is_connected()){pImpl->mSession.close();}
+    try
+    {
+        pImpl->mSession.open(soci::postgresql, connectionString);
+    }
+    catch (const std::exception &e)
+    {
+        throw std::runtime_error("Failed to connect to postgresql with error:\n"
+                               + std::string{e.what()});
+    }
+}
+
+bool PostgreSQL::isConnected() const noexcept
+{
+    return pImpl->mSession.is_connected();
+}
+
+soci::session *PostgreSQL::getSession() const
+{
+    return &pImpl->mSession;
+}
+
+/// Load the connection information
+void PostgreSQL::parseInitializationFile(const std::string &fileName,
+                                         const std::string &section)
+{
+    if (!std::filesystem::exists(fileName))
+    {
+        throw std::runtime_error("Initialization file: " + fileName
+                               + " does not exist");
+    }
+    boost::property_tree::ptree propertyTree;
+    boost::property_tree::ini_parser::read_ini(fileName, propertyTree);
+    // Load the variables with defaults 
+    auto user = std::string(std::getenv("URTS_AQMS_RDONLY_USER"));
+    user = propertyTree.get<std::string> (section + ".user", user);
+    auto password = std::string(std::getenv("URTS_AQMS_RDONLY_PASSWORD"));
+    password = propertyTree.get<std::string> (section + ".password", password);
+    int port = getPort();
+    port = propertyTree.get<int> (section + ".port", getPort());
+    auto address = propertyTree.get<std::string> (section + ".address",
+                                                  getAddress());
+    auto databaseName
+        = propertyTree.get<std::string> (section + ".databaseName");
+    auto applicationName
+        = propertyTree.get<std::string> (section + ".application",
+                                          getApplication()); 
+    // Set information
+    setUser(user);
+    setPassword(password);
+    setPort(port);
+    setDatabaseName(databaseName); 
+    setAddress(address);
+    setApplication(applicationName);
+}
