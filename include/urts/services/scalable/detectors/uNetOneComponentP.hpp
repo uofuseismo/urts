347fecdd7313811a98e261dfc6acfcd830299a6d
Ben Baker <bakerb845@gmail.com>
2024-07-25 13:42:18 -0600

    adding new file

---
+++ include/urts/services/scalable/detectors/uNetOneComponentP.hpp
@@ -0,0 +1,13 @@
+#ifndef URTS_SERVICES_SCALABLE_DETECTORS_UNET_ONE_COMPONENT_P_HPP
+#define URTS_SERVICES_SCALABLE_DETECTORS_UNET_ONE_COMPONENT_P_HPP
+#include <urts/services/scalable/detectors/uNetOneComponentP/inferenceRequest.hpp>
+#include <urts/services/scalable/detectors/uNetOneComponentP/preprocessingRequest.hpp>
+#include <urts/services/scalable/detectors/uNetOneComponentP/processingRequest.hpp>
+#include <urts/services/scalable/detectors/uNetOneComponentP/requestor.hpp>
+#include <urts/services/scalable/detectors/uNetOneComponentP/service.hpp>
+#include <urts/services/scalable/detectors/uNetOneComponentP/inferenceResponse.hpp>
+#include <urts/services/scalable/detectors/uNetOneComponentP/preprocessingResponse.hpp>
+#include <urts/services/scalable/detectors/uNetOneComponentP/processingResponse.hpp>
+#include <urts/services/scalable/detectors/uNetOneComponentP/requestorOptions.hpp>
+#include <urts/services/scalable/detectors/uNetOneComponentP/serviceOptions.hpp>
+#endif
